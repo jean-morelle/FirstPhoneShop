13cae76bb1dfee82f55887751c1e8b9745d2ab95
jean morelle KOUDORO <koudorojeanmorelle408@gmail.com>
2023-07-07 17:27:44 +0200

    modification et creation des lignes des deux commandes

---
+++ FirstPhoneShop/Models/LigneCommande.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstPhoneShop.Models
+{
+    public class LigneCommande
+    {
+        public int CommandeId { get; set; }
+        public int ProduitId { get;set; }
+        public Produit produit { get; set; }
+        public Commande commande { get; set; }
+        public string PrixTotal { get; set; }
+    }
+}
